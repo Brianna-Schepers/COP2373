77b5e23ce2c06b0cbd1ef9d6c715ec4d367c1ec2
Brianna-Schepers <bscheper@student.scf.edu>
2024-07-17 21:22:10 -0400

    Add files via upload

---
+++ BriannaSchepers_Chapter11_B/BriannaSchepers_Chapter11_B.py
@@ -0,0 +1,20 @@
+import math
+from math import cos, radians
+
+
+def get_hypotenuse():
+
+    # Gets the user to input the angle and the length of the adjacent.
+
+    angle = float(input("Enter the nearest angle in degrees: "))
+
+    length = float(input("Enter the length of the adjacent side: "))
+
+    # Uses cos from the math package to calculate the hypotenuse.
+
+    hypotenuse = length/cos(radians(angle))
+
+    print("The hypotenuse is", round(hypotenuse, 3))
+
+
+get_hypotenuse()
